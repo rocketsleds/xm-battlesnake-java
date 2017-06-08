85b0fd159d755b5bdeea3f2b23e57af700bc5995
Tim Thompson <tthompson@xmatters.com>
2017-06-08 15:52:03 -0700

    name snake

--- src/main/java/com/battlesnake/RequestController.java
+++ src/main/java/com/battlesnake/RequestController.java
@@ -38,12 +38,15 @@ public class RequestController {
   @RequestMapping(value="/start", method=RequestMethod.POST, produces="application/json")
   public StartResponse start(@RequestBody StartRequest request) {
     return new StartResponse()
-      .setName("Bowser Snake")
+      .setName("Rocket Snakes")
       .setColor("#FF0000")
+      .setColor("#3335FF")
       .setHeadUrl("http://vignette1.wikia.nocookie.net/nintendo/images/6/61/Bowser_Icon.png/revision/latest?cb=20120820000805&path-prefix=en")
       .setHeadType(HeadType.DEAD)
       .setTailType(TailType.PIXEL)
-      .setTaunt("Roarrrrrrrrr!");
+      .setHeadType(HeadType.SHADES)
+      .setTailType(TailType.FRECKLED)
+      .setTaunt("Boom!");
   }
 
   @RequestMapping(value="/move", method=RequestMethod.POST, produces = "application/json")
@@ -56,7 +59,7 @@ public class RequestController {
     request.getSnakes();
     request.getTurn();
     request.getYou();
-      int [] head = SnakeUtil.getMyHead(
+//      int [] head = SnakeUtil.getMyHead(
 //      SnakeUtil.getAllowableMoves(head,
 //        request.getGameId();
 //      SnakeUtil.
