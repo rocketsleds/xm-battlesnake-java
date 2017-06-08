70fc481efd576f04fff92f710a19d5c9e3563f4c
darylhandley <dhandley@xmatters.com>
2017-06-08 15:05:01 -0700

    none

--- src/main/java/com/battlesnake/SnakeUtil.java
+++ src/main/java/com/battlesnake/SnakeUtil.java
@@ -4,18 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 import com.battlesnake.data.Move;
 import com.battlesnake.data.MoveRequest;
+import com.battlesnake.data.Snake;
 
 public class SnakeUtil {
 
-    public static List<Move> getAllowableMoves(MoveRequest moveRequest) {
+    public static List<Move> getAllowableMoves(int[] position, MoveRequest moveRequest) {
         List<Move> moves = new ArrayList<Move>();
 
         // what's our current location ?
+        int x = position[0];
+        int y = position[1];
 
 
-        // make sure we don't hit the sides
-        // make sure we don't hit another snake
-        // moveRequest.get
+        // create board of allowable moves, all true
+        boolean [][] board = new boolean[moveRequest.getWidth()][moveRequest.getHeight()];
+        for (int i=0; i < moveRequest.getWidth(); i++) {
+            for (int j=0; j < moveRequest.getHeight(); i++) {
+                board[i][j] = true;
+            }
+        }
+
+        // update board for a snake
+        for (Snake snake : moveRequest.getSnakes()) {
+            updateAllowableBoard(board, snake);
+        }
+        for (Snake snake : moveRequest.getDeadSnakes()) {
+            updateAllowableBoard(board, snake);
+        }
+
+        // check left
+        int leftX = x -1;
+        // if (int down
+
+
         return moves;
+    }
+
+    public static void updateAllowableBoard(boolean[][] board, Snake snake) {
+
+        int[][] coords = snake.getCoords();
+        for (int i=0; i < coords.length; i++) {
+            int[] position = coords[i];
+            board[position[0]] [position[1]] = false;
+        }
+
+    }
+
+    public static int[] getMyHead(MoveRequest moveRequest) {
+        List<Snake> snakes = moveRequest.getSnakes();
+
+        for (Snake snake : snakes) {
+            if (snake.getId() == moveRequest.getYou()) {
+                return snake.getCoords()[0];
+            }
+        }
+        throw new RuntimeException("Invalid setup");
     }
 }
--- src/main/java/com/battlesnake/test/SnakeUtilTest.java
+++ src/main/java/com/battlesnake/test/SnakeUtilTest.java
@@ -1,9 +1,14 @@
 package com.battlesnake.test;
 
+import java.util.ArrayList;
+import com.battlesnake.SnakeUtil;
+import com.battlesnake.data.MoveRequest;
+import com.battlesnake.data.Snake;
 import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4ClassRunner.class)
@@ -13,5 +18,27 @@ public class SnakeUtilTest {
     public void testGetAllowableMoves() {
         assertTrue(true);
 
+    }
+
+
+    @Test
+    public void testGetMyHead() {
+        String myId = "MY_ID";
+
+        MoveRequest testMoveRequest = new MoveRequest();
+
+        ArrayList<Snake> snakes = new ArrayList<Snake>();
+        testMoveRequest.setSnakes(snakes);
+        testMoveRequest.setYou(myId);
+        Snake snake = new Snake();
+        snakes.add(snake);
+
+        snake.setId(myId);
+        int[][] coords = {{5,6}, {3,4}, {5,6}};
+        snake.setCoords(coords);
+
+        int [] head = SnakeUtil.getMyHead(testMoveRequest);
+        assertEquals(5, head[0]);
+        assertEquals(6, head[1]);
     }
 }
